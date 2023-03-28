1db1efbe5446112c5da015f69326e549dc7b153d
nromanen <nromanen.softserveinc@gmail.com>
2023-03-28 12:34:40 +0300

    Project structure

---
+++ src/test/java/model/Test1.java
@@ -0,0 +1,13 @@
+package model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class Test1 {
+
+    @Test
+    public void test1(){
+        assertTrue(false);
+    }
+}
